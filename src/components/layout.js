65badc09a984fdca941dbf5f22935c88fc5937be
Your NameZachGaskin <zgaskin@ufclab.com>
2019-11-14 15:13:56 -0500

    nothing good

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import {
+  IdentityContextProvider,
+  useNetlifyIdentity,
+} from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  const identity = useNetlifyIdentity("https://authsite.netlify.com/")
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,22 +25,24 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <IdentityContextProvider value={identity}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </IdentityContextProvider>
     </>
   )
 }
